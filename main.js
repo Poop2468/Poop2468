c1f00ee39cf17fb2648700c09d46706ce192bd07
Poop2468 <99230532+Poop2468@users.noreply.github.com>
2025-10-30 16:05:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('cute3d'), alpha: true });
+renderer.setSize(window.innerWidth, 300);
+
 // 🌟 로딩 화면
 window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
